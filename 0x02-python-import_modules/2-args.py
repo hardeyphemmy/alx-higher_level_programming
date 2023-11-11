ac2fb1acad34173403f7a6c273f9dc3ae8236cef
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-11 10:15:00 +0000

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+length = len(sys.argv)
+if (length == 1):
+    print("1 argument:")
+elif (length > 1):
+    print("{} arguments:".format(length - 1))
+else:
+    print("0 arguments.")
+
+for i in range(1, length):
+    print("{}: {}".format(i, sys.argv[i]))
+
+
+
+
+
+
+
+
+
+
+
+
