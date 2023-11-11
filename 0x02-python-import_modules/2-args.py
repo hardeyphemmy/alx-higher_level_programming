721a2a08d094b9cabc5391b5ba8bd99f20ac91f1
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-11 10:55:52 +0000

    arguments change

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-length = len(sys.argv)
-if (length == 2):
-    print("1 argument:")
-elif (length > 2):
-    print("{} arguments:".format(length - 1))
-else:
-    print("0 arguments.")
+    length = len(sys.argv)
+    if (length == 2):
+        print("1 argument:")
+    elif (length > 2):
+        print("{} arguments:".format(length - 1))
+    else:
+        print("0 arguments.")
 
-for i in range(1, length):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, length):
+        print("{}: {}".format(i, sys.argv[i]))
