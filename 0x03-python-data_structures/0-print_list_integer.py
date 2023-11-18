afdbd0eb1d28485ed834e41a44fcd6af82b58df3
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-18 00:45:28 +0000

    print integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for number in my_list:
-        if isinstance(number, int):
-            print(["{:d}"].format(list), end=', ')
-    print()
-
-
 if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
+    def print_list_integer(my_list=[]):
+        for number in my_list:
+            if isinstance(number, int):
+                print(["{:d}"].format(number), end=', ')
+        print()
+
+
+my_numbers = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
