9a6b669c6f44f58d105a123deb4977b92a9b00d7
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-20 23:00:16 +0000

    Replace a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -11,7 +11,6 @@ def replace_in_list(my_list, idx, element):
     """
 
     if 0 <= idx < len(my_list):
-        my_list_copy = my_list.copy()
         return (my_list)
     else:
         return (my_list)
