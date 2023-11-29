2f986a6edb16fe0444fa8896679962d0a1022dc9
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-29 10:37:46 +0000

    Add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,13 +3,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     Function that adds two tuples
     Args:
-    Tuple_a: Element in the First arg
-    Tuble_b: Element in the second arg
+    tuple_a: Element in the First arg
+    tuble_b: Element in the second arg
     Return:
     Tuple with 2 integers
     """
     add_tuple = tuple(a + b for a, b in zip(tuple_a, tuple_b))
     print(add_tuple)
+    if add_tuple[0] < 2:
+        return 0
+    else:
+        return sum(add_tuple)
 
 
 if __name__ == "__main__":
