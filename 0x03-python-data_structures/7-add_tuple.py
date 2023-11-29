d701a6eb4ba284e643cd69ae697b3d9a06ddad24
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-29 10:53:18 +0000

    Add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,6 +10,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     result_tuple = tuple(a + b for a, b in zip(tuple_a, tuple_b))
     print(result_tuple)
+    
     if result_tuple[0] < 2:
         return (0, 0)
     else:
@@ -20,4 +21,3 @@ if __name__ == "__main__":
     tuple_a = (1, 89)
     tuple_b = (88, 11)
     result = add_tuple(tuple_a, tuple_b)
-    print(result)
