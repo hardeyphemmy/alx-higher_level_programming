8a5fb90af5743fa3d8d79e9bbd522d85a080f8ef
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-30 18:39:10 +0000

    Add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,20 +12,24 @@ def add_tuple(tuple_a=(), tuple_b=()):
     print(result_tuple)
 
     # check the length of tuple
-    if len(tuple_a) > 2:
-        tuple_a = tuple_a[:2]
+    element_a = tuple_a[0] if len(tuple_a) >= 1 else 0
+    element_b = tuple_b[0] if len(tuple_b) >= 1 else 0
 
-    if len(tuple_b) > 2:
-        tuple_b = tuple_b[:2]
+    element_c = tuple_a[1] if len(tuple_a) > 2 else 2
+    element_d = tuple_b[1] if len(tuple_b) > 2 else 2
+
+    result_tuple = (element_a + element_b, element_c + element_d)
 
     # if a tuple is smaller than 2, use the value 0 for each missing integer
-    if any(x < 2 for x in tuple_a or any(x < 2 for x in tuple_b)):
-        return (0, 0)
+    """if any(x < 2 for x in tuple_a or any(x < 2 for x in tuple_b)):
+        return (0)
     else:
-        return (result_tuple)
+        return (result_tuple)"""
+
 
 
 if __name__ == "__main__":
     tuple_a = (1, 89)
     tuple_b = (88, 11)
     result = add_tuple(tuple_a, tuple_b)
+
