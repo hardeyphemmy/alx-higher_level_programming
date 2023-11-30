4a98f3f77a17f5a64c95990075d67b52fe49657e
hardeyphemmy <adegboyefemi104@gmail.com>
2023-11-30 21:03:12 +0000

    Max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    function that print the biggest integer
+    Return:
+    If list is empty, None
+    """
+    max_number = my_list[0]
+    for number in my_list[1:]:
+        if number > max_number:
+            max_number = number
+
+        return (max_number)
+
+
+if __name__ == "__main__":
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    max_number = max_integer(my_list)
+
+    print("Max: {}".format(max_number))
