7242d831b977ff9d94de13494740bde17ab0d739
hardeyphemmy <adegboyefemi104@gmail.com>
2023-12-24 23:07:30 +0000

    Safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, IndexError):
+        print("Error: ValueError or IndexError occurred")
+        return False
+    return value
+
+
+if __name__ == "__main__":
+    value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
