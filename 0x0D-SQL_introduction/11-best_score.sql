5291a17638b5ed3ccf2ff1558c419f6af4c7decd
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-17 10:59:07 +0000

    best score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- list all records with score > 10in the second_tablw
+-- Display both score and name in descending order of score
+
+SELECT score, name FROM second_table WHERE score > 10 ORDER BY score DESC;
