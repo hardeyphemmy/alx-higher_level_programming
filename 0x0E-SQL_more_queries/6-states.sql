466fb838f1aa456a35c5dc62e27886b487e92831
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-17 23:38:48 +0000

    state table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create database htbn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- create table states
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
