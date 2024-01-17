f9bd9819a4759d23319995e844d45d62f6c4c3db
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-17 22:18:26 +0000

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- user should have all priviledges password 
+-- user should have all privileges password 
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- all privileges to servers
-GRANT ALL PRIVILEDGES ON *.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
