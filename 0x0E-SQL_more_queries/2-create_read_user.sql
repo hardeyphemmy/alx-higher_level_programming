0d95fbdff6323e8379ce5225e8eb2ca6984853c6
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-17 22:37:09 +0000

    create read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create database and user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_psd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_psd';
 -- grant user_0d_2  SELECT privilege
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
