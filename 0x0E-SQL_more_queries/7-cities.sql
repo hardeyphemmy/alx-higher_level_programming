8400d344343f9a1c442ae0a62e42db4eb481a4cb
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-18 00:15:19 +0000

    cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- use database
+USE hbtn_0d_usa;
+
+-- create table cities
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id), 
+FOREIGN KEY(state_id) REFERENCE states(id));
