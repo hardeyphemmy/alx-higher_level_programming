7162f8d990b65cf51e94e39c416b67dccbe60ad4
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-18 22:30:26 +0000

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- list all genres from database and display the number of shows linkes
--- list all rows of  a database meeting condition
+-- list all rows of  a database meeting the condition
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
