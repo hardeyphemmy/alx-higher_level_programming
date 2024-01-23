2d28a7a30d8cb057f7271b6682215d825ce0c225
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-23 08:29:24 +0000

    create state and cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,12 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- use database
 USE hbtn_0d_usa;
+SHOW TABLES;
+
+-- creat table state
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id));
 
 -- create table cities
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT, state_id INT NOT NULL,
