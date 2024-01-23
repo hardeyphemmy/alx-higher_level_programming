a7548f47d6076bc11a36c79c61365b04532e5b8b
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-23 11:54:10 +0000

    Genre show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_tv_shows;
 -- use hbtn_0d_tvshows
 USE hbtn_0d_tv_shows;
 -- list all rows in a column
-SELECT tv_show.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
