d3f7ad269db3afe5fedab77680b03a2e5f759a9f
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-23 11:56:06 +0000

    Genre show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- list all shows in hbtn_0d_tvshows that have atleast one genre linked
 -- create databasedd 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tv_shows;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_tv_shows.tv_shows;
 -- use hbtn_0d_tvshows
-USE hbtn_0d_tv_shows;
+USE hbtn_0d_tv_shows.tv_shows;
 -- list all rows in a column
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres
