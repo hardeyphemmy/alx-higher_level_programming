b07d4a4a66f6019b9d8b25bd093e9650f0471036
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-24 21:37:22 +0000

    create first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a table called 'first_table' in the current database
-CREATE IF NOT EXISTS hbtn_test_db_4;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 -- switch database
 USE hbtn_test_db_4;
 -- create table
