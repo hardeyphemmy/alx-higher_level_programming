4f41a6e2b016eef498e987e82660261e744ac484
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-24 21:19:56 +0000

    create first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 -- Create a table called 'first_table' in the current database
--- 'first_table' should have (id INT) and name VARCHAR(256))
-
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- create table
 CREATE TABLE IF NOT EXISTS first_table (id INT, and name VARCHAR(256));
