4e0f069fa9b0238b3e8e6509928a73e9c1b61936
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-27 22:15:59 +0000

    Table first

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 -- switch database
 USE hbtn_test_db_4;
 -- create table
-CREATE TABLE IF NOT EXISTS first_table (id INT, and name VARCHAR(256));
+CREATE TABLE IF NOTS EXISTS first_table (id INT, and name VARCHAR(256));
