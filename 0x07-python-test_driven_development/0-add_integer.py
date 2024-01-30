e4f2242516d3b96ed9aec2a23b9be3dedf4f86f9
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-30 01:15:07 +0000

    Add integr

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,7 @@
 def add_integer(a, b=98):
     """The methods that add two numbers.
 
+    Args:
     a and b must be integers or floats, a and b must be first
     casted to integers if they are float.
 
