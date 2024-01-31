75dd5660d67b959edbbbb4c30cdea2bc7ee27037
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-31 22:36:22 +0000

    Real defination of rect

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/use/bin/python3
+"""This is a module of a rectangle."""
+
+
+class Rectangle:
+    """
+    This is a class of a rectangle
+
+    Attribute:
+    width: The length of the rectangle
+    height: The breath of the rectangle
+
+    Arg:
+    value: The integer assigned
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Property to get the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property to set the width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property to get the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property to set the height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
