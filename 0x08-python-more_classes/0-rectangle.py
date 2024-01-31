f53edcfc1bba7702c6ac773c33a24144c6f48e7f
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-31 21:53:19 +0000

    Just Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """This is an empty class."""
     pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
