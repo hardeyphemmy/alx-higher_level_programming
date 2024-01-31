b1088bb77392fc570a284c17aafc81ce5b85037b
hardeyphemmy <adegboyefemi104@gmail.com>
2024-01-31 23:03:17 +0000

    Real defination of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -50,9 +50,9 @@ if __name__ == "__main__":
     try:
         my_rectangle1 = Rectangle(2, -3)
     except Exception as e:
-        print("[{}] {}".format(e.__class__.name__, e))
+        print("[{}] {}".format(e.__class__.__name__, e))
 
     try:
         my_rectangle2 = Rectangle(2, "3")
     except Exception as e:
-        print("[{}] {}".format(e.__class__.name__, e))
+        print("[{}] {}".format(e.__class__.__name__, e))
