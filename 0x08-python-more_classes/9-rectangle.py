d9588e5f2ee937427cbf051a06abb936ce1e2ce0
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-01 14:53:43 +0000

    A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -90,7 +90,7 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1 == rect_2:
-            return rect_1 
+            return rect_1
 
         area_1 = rect_1.area()
         area_2 = rect_2.area()
