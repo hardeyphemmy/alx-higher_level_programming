c012c67ce8b91408f1dd6caf144bbe9a79730666
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-12 13:38:50 +0000

    Java and fix semisatandard using consolelog with single quote

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 // print - script that print three lines
 
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript is amazing")
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
