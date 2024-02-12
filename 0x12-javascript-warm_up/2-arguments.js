0ea2e268ea9c7159d59db8d9669f85c4fe51f5f6
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-12 14:00:36 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//print message depending on args passed
+
+const myVar = process.argv.length - 2;
+if (myVar === 0) {
+	console.log('No argument');
+} else if (myVar === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
