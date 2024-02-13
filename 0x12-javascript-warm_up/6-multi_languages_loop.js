2096b740ea5e5e06051ee3f2ff678d04825e72b1
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-13 09:35:06 +0000

    print multiple lines with a loop and array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// print multiple line using an array of strings and a loop
+
+const lines = [
+	"C is fun",
+	"Python is cool",
+	"JavaScript is amazing"
+]
+
+for (let i = 0; i < lines.length; i++) {
+	console.log(lines[i]);
+}
