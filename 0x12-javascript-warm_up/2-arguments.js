0e8a814c6ff724adab2ee58c1ab1fcc5140bb1e0
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-13 08:33:28 +0000

    Remove excess space in console many arg passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-//print message depending on args passed
+// print message depending on args passed
 
 const myVar = process.argv.length - 2;
 if (myVar === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (myVar === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
