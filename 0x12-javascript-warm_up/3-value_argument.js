9ed1e644fd4b68adea59e1540fc5f1389f46c999
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-13 08:35:28 +0000

    print first arg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 // print the first arg passed
 
 if (!process.argv[2]) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
