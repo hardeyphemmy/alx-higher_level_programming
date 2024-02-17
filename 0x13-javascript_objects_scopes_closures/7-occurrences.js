1051668d2db5012aa35c59038c04ff34937bfbcc
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-17 19:14:28 +0000

    Number of occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Return the number of occurrences in a list
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
 	// initialize the variable to count
 	let count = 0;
 	// Loop throughh the list
 	for (let i = 0; i < list.length; i ++) {
 		// check if current element is equal to a search element
-		if (list[i] === serachElement) {
+		if (list[i] === searchElement) {
 			// Increment count
 			count++;
 		}
