37a4789671406079a679f6075d7050fca9afaa71
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-17 11:50:08 +0000

    square charprint

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,54 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return 'Rectangle {}';
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  // method to print rectangle using x char
+  print () {
+    // iterate each row
+    for (let i = 0; i < this.height; i++) {
+      // create string x repeated 'this.width' times
+      const row = 'X'.repeat(this.width);
+      // print row
+      console.log(row);
+    }
+  }
+
+  // rotate method to exchange the width and rectangle
+  rotate () {
+    // swap the values of width and height
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  // double method that multiplies the width and height by 2
+  double () {
+    // multiply width and height by 2
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+// class square extends Rectangle
+class Square extends Rectangle {
+  constructor (size) {
+    // call constructor of the parent class with size
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
