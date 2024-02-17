afc22ba36ab2cc4d8098f2c5b6bc3fbb619a51cd
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-17 12:18:05 +0000

    square charprint using parentSquare

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,5 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return 'Rectangle {}';
-    }
-    this.width = w;
-    this.height = h;
-  }
-
-  // method to print rectangle using x char
-  print () {
-    // iterate each row
-    for (let i = 0; i < this.height; i++) {
-      // create string x repeated 'this.width' times
-      const row = 'X'.repeat(this.width);
-      // print row
-      console.log(row);
-    }
-  }
-
-  // rotate method to exchange the width and rectangle
-  rotate () {
-    // swap the values of width and height
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  // double method that multiplies the width and height by 2
-  double () {
-    // multiply width and height by 2
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
 // Import Square from 5-square
 const ParentSquare = require('./5-square');
 
