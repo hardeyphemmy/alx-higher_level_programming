e5134639383b5a9e7e47d809e882794f206645fa
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-19 22:01:31 +0000

    conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 // export a num to base 10
 exports.converter = function (number, base) 
-// Define char to represent digits beyond 9
-const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// initialize result as an empty string
-const result = '';
-// perform conversion
-while (number > 0) {
-const remainder = number % base;
-result = digits[remainder] + result;
+	// Define char to represent digits beyond 9
+	const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	// initialize result as an empty string
+	const result = '';
+	// perform conversion
+	while (number > 0) {
+		const remainder = number % base;
+		result = digits[remainder] + result;
 	
-// update for next iteration
-number = Math.floor(number / base);
-}
-// return converted number
- return result || '0';
+		// update for next iteration
+		number = Math.floor(number / base);
+	}
+	// return converted number
+	 return result || '0';
 };
