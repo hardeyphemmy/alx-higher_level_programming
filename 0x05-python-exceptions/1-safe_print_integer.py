fa40448e076fd84428772331c7f601864ea62971
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-20 09:27:12 +0000

    print with an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,7 +10,8 @@ def safe_print_integer(value):
 
 
 if __name__ == "__main__":
-    value = 89
+
+    value = {'89'}
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
