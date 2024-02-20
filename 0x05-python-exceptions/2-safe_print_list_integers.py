617a7831f6e0c47cf694e8698cf4e71decaedba4
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-20 12:46:31 +0000

    safe print integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             else:
                 break
-    except (ValueError):
+    except (ValueError, TypeError):
         pass
     return count
 
