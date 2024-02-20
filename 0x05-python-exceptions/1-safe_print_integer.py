ca20be1f8e67f210fc7e394395c3f8447d7eb756
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-20 09:45:47 +0000

    print with an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,13 +5,12 @@ def safe_print_integer(value):
         print("{:d}".format(arg))
         return True
     except (ValueError, IndexError):
-        print("Error: ValueError or IndexError occurred")
         return False
 
 
 if __name__ == "__main__":
 
-    value = {'89'}
+    value = 89
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
@@ -21,7 +20,7 @@ if __name__ == "__main__":
     if not has_been_print:
         print("{} is not an integer".format(value))
 
-    value = "School"
+    value = 'School'
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
