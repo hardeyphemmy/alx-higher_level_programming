cf2da45ea1e54774679ecc18e9b65c19e84aeb60
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-21 23:41:11 +0000

    delete key in dict

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -5,6 +5,7 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
     return a_dictionary
 
+
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
