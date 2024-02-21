d59fcc08e6c0889e6b2b6c8cfab58579448b8b87
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-21 16:14:02 +0000

    print division on finally

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Inside result: None")
+    else:
+        print("Inside result: {}".format(result))
+    finally:
+        return result
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
