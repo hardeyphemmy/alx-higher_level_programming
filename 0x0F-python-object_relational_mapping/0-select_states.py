24c75eae9c7ac1d98caee837259cfac5fdf201fb
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-22 14:38:39 +0000

    select states from fetch all adjusted

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,35 +3,37 @@
 import sys
 import MySQLdb
 
-# checks if the number of argument is correct
+# Check if the number of arguments is correct
 if len(sys.argv) != 4:
     print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
     sys.exit(1)
-# Extract credentials username, password, database = sys.argv[1:2]
+
+# Extract credentials
+username, password, database = sys.argv[1:]
 
 try:
-    # connect to MySQL database
+    # Connect to MySQL database
     db = MySQLdb.connect(host="localhost",
-                        user="root",
-                        passwd="Phemmygmail@104",
-                        db="hbtn_0e_0_usa",
-                        port=3306)
+                         user="root",
+                         passwd="Phemmygmail@104",
+                         db="hbtn_0e_0_usa",
+                         port=3306)
 
-    # create a cursor object to execute queries
+    # Create a cursor object to execute queries
     cursor = db.cursor()
 
     # Execute SQL query to retrieve all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # FETCH all rows from the result set
-    rows = cursor.fetchall()
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()[:5]
 
-    # print the list of states
+    # Print the list of states
     print("List of states:")
-    for row in rows[:5]:
-            print("({}, '{}')".format(row[0], row[1]))
+    for row in rows:
+        print(row)
 
-    # close cursor and database connection
+    # Close cursor and database connection
     cursor.close()
     db.close()
 
