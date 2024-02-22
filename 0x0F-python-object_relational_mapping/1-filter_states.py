98a421a52d3d70af9a2a03c2b721c5327434f70f
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-22 19:47:17 +0000

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This is filter module."""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """Check correct number of arguments"""
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        return
+
+    """Extract credentials."""
+    username, password, database = sys.argv[1:]
+
+    try:
+        """Connect to MySQL database."""
+        db = MySQLdb.connect(host="localhost",
+                             user=username,
+                             passwd=password,
+                             db=database,
+                             port=3306)
+        """Create cursor object."""
+        cur = db.cursor()
+        """Execute SQL query."""
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER\
+                    BY id ASC")
+        """Fetch all rows."""
+        rows = cur.fetchall()
+        """Print list of states."""
+        print("List of states:")
+        for row in rows:
+            print(row)
+
+        """Close cursor and database."""
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
