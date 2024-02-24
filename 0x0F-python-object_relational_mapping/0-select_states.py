757cd9996c941bef0cbdc4d27a16c42a019daac8
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-24 09:05:59 +0000

    select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,35 +7,33 @@ import MySQLdb
 if len(sys.argv) != 4:
     print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
     sys.exit(1)
+    # Extract credentials
+    username, password, database = sys.argv[1:]
+    try:
+        # Connect to MySQL database
+        db = MySQLdb.connect(host="localhost",
+                            user="root",
+                            passwd="Phemmygmail@104",
+                            db="hbtn_0e_0_usa",
+                            port=3306)
 
-# Extract credentials
-username, password, database = sys.argv[1:]
+        # Create a cursor object to execute queries
+        cursor = db.cursor()
 
-try:
-    # Connect to MySQL database
-    db = MySQLdb.connect(host="localhost",
-                         user="root",
-                         passwd="Phemmygmail@104",
-                         db="hbtn_0e_0_usa",
-                         port=3306)
+        # Execute SQL query to retrieve all states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()[:5]
 
-    # Execute SQL query to retrieve all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Print the list of states
+        print("List of states:")
+        for row in rows:
+            print(row)
 
-    # Fetch all rows from the result set
-    rows = cursor.fetchall()[:5]
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
 
-    # Print the list of states
-    print("List of states:")
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
-
-except MySQLdb.Error as e:
-    print("MySQL Error:", e)
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
