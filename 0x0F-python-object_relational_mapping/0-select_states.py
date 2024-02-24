4ba8cb07c191a1f7696f75c198770d358e918476
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-24 09:13:19 +0000

    select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,16 @@ import MySQLdb
 if len(sys.argv) != 4:
     print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
     sys.exit(1)
+
     # Extract credentials
     username, password, database = sys.argv[1:]
     try:
         # Connect to MySQL database
         db = MySQLdb.connect(host="localhost",
-                            user="root",
-                            passwd="Phemmygmail@104",
-                            db="hbtn_0e_0_usa",
-                            port=3306)
+                             user="root",
+                             passwd="Phemmygmail@104",
+                             db="hbtn_0e_0_usa",
+                             port=3306)
 
         # Create a cursor object to execute queries
         cursor = db.cursor()
