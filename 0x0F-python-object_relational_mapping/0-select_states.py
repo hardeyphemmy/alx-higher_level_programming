ba3f079abe92555b732cbf7bf6a26c70eb55729e
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-24 10:22:32 +0000

    select all states and define main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,36 +3,36 @@
 import sys
 import MySQLdb
 
-# Check if the number of arguments is correct
+"""Check if the number of arguments is correct"""
 if len(sys.argv) != 4:
     print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
     sys.exit(1)
 
-    # Extract credentials
+    """Extract credentials"""
     username, password, database = sys.argv[1:]
     try:
-        # Connect to MySQL database
+        """Connect to MySQL database"""
         db = MySQLdb.connect(host="localhost",
                              user="root",
                              passwd="Phemmygmail@104",
                              db="hbtn_0e_0_usa",
                              port=3306)
 
-        # Create a cursor object to execute queries
+        """ Create a cursor object to execute queries"""
         cursor = db.cursor()
 
-        # Execute SQL query to retrieve all states
+        """ Execute SQL query to retrieve all states"""
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Fetch all rows from the result set
+        """ Fetch all rows from the result set"""
         rows = cursor.fetchall()
 
-        # Print the list of states
+        """ Print the list of states"""
         print("List of states:")
         for row in rows:
             print(row)
 
-        # Close cursor and database connection
+        """ Close cursor and database connection"""
         cursor.close()
         db.close()
 
