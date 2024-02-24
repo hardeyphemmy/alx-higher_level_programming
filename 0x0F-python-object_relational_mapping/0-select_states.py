9cd9922fae48ff844d1e861d2fbcf987d1f68c66
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-24 10:11:02 +0000

    select all states and define main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,44 +3,38 @@
 import sys
 import MySQLdb
 
+# Check if the number of arguments is correct
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    sys.exit(1)
 
-def main():
-    # Check if the number of arguments is correct
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    # Extract credentials
+    username, password, database = sys.argv[1:]
+    try:
+        # Connect to MySQL database
+        db = MySQLdb.connect(host="localhost",
+                             user="root",
+                             passwd="Phemmygmail@104",
+                             db="hbtn_0e_0_usa",
+                             port=3306)
 
-        # Extract credentials
-        username, password, database = sys.argv[1:]
-        try:
-            # Connect to MySQL database
-            db = MySQLdb.connect(host="localhost",
-                                 user="root",
-                                 passwd="Phemmygmail@104",
-                                 db="hbtn_0e_0_usa",
-                                 port=3306)
+        # Create a cursor object to execute queries
+        cursor = db.cursor()
 
-            # Create a cursor object to execute queries
-            cursor = db.cursor()
+        # Execute SQL query to retrieve all states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-            # Execute SQL query to retrieve all states
-            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()[:5]
 
-            # Fetch all rows from the result set
-            rows = cursor.fetchall()[:5]
+        # Print the list of states
+        print("List of states:")
+        for row in rows:
+            print(row)
 
-            # Print the list of states
-            print("List of states:")
-            for row in rows:
-                print(row)
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
 
-            # Close cursor and database connection
-            cursor.close()
-            db.close()
-
-        except MySQLdb.Error as e:
-            print("MySQL Error:", e)
-
-
-if __name__ == "__main__":
-    main()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
