8a74a6ae846c8708a05ec9b3bd8e2ef1cc67c190
hardeyphemmy <adegboyefemi104@gmail.com>
2024-03-04 16:10:15 +0000

    Additions of integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,33 +6,11 @@ def add_integer(a, b=98):
     a and b must be integers or floats, a and b must be first
     casted to integers if they are float.
 
-    Returns
-    an integer: the sum of a and b."""
+    Raises
+    TypeError: If either of a or b is a non-integer and non-float."""
 
-    if not (isinstance(a, int)):
-        raise TypeError("'a' must be an integer") and not\
-                (isinstance(a, float)):
-            raise TypeError("'a' must be a float")
-    if not (isinstance(b, int)):
-        raise TypeError("'b' must be an integer") and not\
-                (isinstance(b, float)):
-            raise TypeError("'b' must be a float")
-    # convert a and b to integer
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-    return (a + b)
-
-
-if __name__ == "__main__":
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("'a' must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("'b' must be an integer")
+    return (int(a)) + (int(b))
