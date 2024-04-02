144d90235235d10b840565f05b72be492392ae3a
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-02 13:41:08 +0000

    text indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,7 +14,7 @@ def text_indentation(text):
     if not (isinstance(text, str)):
         raise TypeError("text must be a string")
 
-        puntuation_marks = {'.', '?', ':'}
+        punctuation_marks = {'.', '?', ':'}
         start_of_line = True
 
         for char in text:
