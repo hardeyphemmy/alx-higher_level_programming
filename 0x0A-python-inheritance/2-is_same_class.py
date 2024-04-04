ccb57232e64c10caff3a61876d41867cd22758a8
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-04 11:37:45 +0000

    same instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""The module defines a class checking function"""
+
+
+def is_same_class(obj, a_class):
+    """
+    This function checks if an object is exactly an instance of a given class
+
+    Args:
+        obj(any): The object to check
+        a_class(type): The class to match the type of obj of an instance
+    Returns:
+        If obj is exaclty an instance of a class
+        False otherwise
+    """
+
+    if type(obj) == a_class:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    is_same_class = __import__('2-is_same_class').is_same_class
+
+    a = 1
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+    if is_same_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
