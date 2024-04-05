39ca09278b20c87c7f41b31a2ac9d00613ffc19f
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-05 16:50:42 +0000

    validate value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     """
 
     def area(self):
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
 
 
     def integer_validator(self, name, value):
