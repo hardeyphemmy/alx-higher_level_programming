4bbe2690fbbb168410fa0b2f5b7e6b1a336269ea
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-05 20:03:38 +0000

    BaseGeometry inherits Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""The module defines the BaseGeometry"""
+
+
+class BaseGeometry:
+    """The class function of BaseGeometry.
+
+    Calculate the area.
+
+    Raises:
+        Exception: If the area calculation is not impemented.
+    """
+
+    def __init__(self):
+        """Initialize the BaseGeometry instance."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates the value making sure it is an integer.
+
+        Args:
+            name(str): The name of the value
+            value(any): The value to validate
+
+        Raises:
+            TypeError exception if not an integer
+            ValueError exception if not greater than or equal to 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """The class Rectangle inherited from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance with width and height.
+
+        Args:
+            width(int): The width of the rectangle.
+            height(int): The height of a rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of the rectangle
+        Retrns:
+            int: Area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+if __name__ == "__main__":
+    Rectangle = __import__('8-rectangle').Rectangle
+
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
