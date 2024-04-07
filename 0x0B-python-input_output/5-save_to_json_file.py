7ce36933371350e7aafd8de28aea731b7a8b3a1c
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-07 23:20:33 +0000

    save python to a json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module write and save a file using json"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    This functions writes an object to a text file
+    Args:
+        my_obj(str): The object in a file
+        filename(str0: The file to be save
+    """
+    if isinstance(my_obj, set):
+        my_obj = list(my_obj)
+
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    filename = "my_list.json"
+    my_list = [1, 2, 3]
+    save_to_json_file(my_list, filename)
+
+    filename = "my_dict.json"
+    my_dict = {
+        'id': 12,
+        'name': "John",
+        'places': ["San Francisco", "Tokyo"],
+        'is_active': True,
+        'info': {
+            'age': 36,
+            'average': 3.14
+        }
+    }
+    save_to_json_file(my_dict, filename)
+
+    try:
+        filename = "my_set.json"
+        my_set = {132, 3}
+        save_to_json_file(my_set, filename)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
