2e0fec5a39ebaeabb9d575d98ed92a5fcd7168a4
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-07 16:59:22 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    read a text file UTF8 and prints it to stdout
+    Args:
+        filename(str): The name of the file to read
+    Raises:
+        FileNotFoundError: If the exact file do not exist
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
+
+
+if __name__ == "__main__":
+    read_file = __import__('0-read_file').read_file
+
+    read_file("my_file_0.txt")
+
+    print("\nWe offer a truly innovative approach to education:\n"
+    "focus on building reliable applications and scalable systems,\n"
+    "take on real-world challenges, collaborate with your peers.\n\n"
+
+    "A school every software engineer would have dreamt of!")
