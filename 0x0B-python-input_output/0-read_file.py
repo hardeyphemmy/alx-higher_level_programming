fedfdd135a1d208ca3f3ba5d39349433e1f7ae53
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-07 17:24:22 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The module defines file to read."""
 
+
 def read_file(filename=""):
     """
     read a text file UTF8 and prints it to stdout
@@ -21,7 +22,7 @@ if __name__ == "__main__":
     read_file("my_file_0.txt")
 
     print("\nWe offer a truly innovative approach to education:\n"
-    "focus on building reliable applications and scalable systems,\n"
-    "take on real-world challenges, collaborate with your peers.\n\n"
+          "focus on building reliable applications and scalable systems,\n"
+          "take on real-world challenges, collaborate with your peers.\n\n"
 
-    "A school every software engineer would have dreamt of!")
+          "A school every software engineer would have dreamt of!")
