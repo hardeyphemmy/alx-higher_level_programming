d46d6b85511295e838a9bec996ccdb5ce97e0056
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-17 10:16:43 +0000

    append to search

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""The module updates a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function inserts in  middle of a file
+    Args:
+        filename(str): name of the file
+        search_string(str): the string to search within the file
+        new_string(str): the new string to append
+    Return:
+        None
+    """
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+
+        with open(filename, "w") as file:
+            for line in lines:
+                file.write(line)
+                if search_string in line:
+                    file.write(new_string + "\n")
+    except FileNotFoundError:
+        print(f"Error: file '{filename}' not found")
