86e7b393c761a0f8c89b284357c71a610f86da65
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-27 22:13:30 +0000

    rectangle corrected value

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(Base):
         """Width private instance attribute setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
