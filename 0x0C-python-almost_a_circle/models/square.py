b481c3dba7d0af6a0d2b0f88ffb37c4c881c9fa7
hardeyphemmy <adegboyefemi104@gmail.com>
2024-04-29 22:43:34 +0000

    the square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""The model Square inherited from Rectangle."""
+
+from models.rectangle import Rectangle
+class Square(Rectangle):
+    """The class inherit from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """The function initialize a square
+        Args:
+            size(int): The size of the square
+            x(int): The x-coordinate of the square
+            y(int): The y-coordinate of the square
+            id(int): The identifier of the square
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """square getter size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set the size of the square"""
+        self.width = value
+        self.height = value
+
+    def _str_(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.size)
+
