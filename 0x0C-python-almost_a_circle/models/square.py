31079c61626e878239499c9f03d9087dc025818b
hardeyphemmy <adegboyefemi104@gmail.com>
2024-05-02 12:13:00 +0000

    Add base class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@
 from models.rectangle import Rectangle
 
 
-class Square(Base):
+class Square(Rectangle):
     """The class inherit from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
